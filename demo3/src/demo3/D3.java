6be8ff8d35d1b72a7f5c3505d2cce39d47bf7fdd
jixiaofei <28819948@qq.com>
2019-03-23 09:28:02 +0800

    d5

--- demo3/src/demo3/D3.java
+++ demo3/src/demo3/D3.java
@@ -4,7 +4,7 @@ public class D3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello D3 d4!");
+		System.out.println("Hello D3 d4");
 
 	}
 
